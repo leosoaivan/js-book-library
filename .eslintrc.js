7664b389062199f957bbe033e10d76fa83b0be55
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-05-23 07:31:08 -0400

    Replace read button with icon

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
             {
                 "skipBlankLines": true
             }
-        ]
+        ],
+        "max-len": "warn"
     }
 };
--- src/js/main.js
+++ src/js/main.js
@@ -11,11 +11,11 @@ function Book(title, author, pages, read) {
   };
 };
 
-function Button(type, btnText, btnClass) {
-  this.type = type;
-  this.btnText = btnText;
-  this.btnClass = btnClass;
-};
+// function Button(type, btnText, btnClass) {
+//   this.type = type;
+//   this.btnText = btnText;
+//   this.btnClass = btnClass;
+// };
 
 /**
  * Variables
@@ -33,9 +33,6 @@ let book2 = new Book('The Two Towers', 'J. R. R. Tolkien', '415', true);
 let book3 = new Book('The Return of the King', 'J. R. R. Tolkien', '347', true);
 let book4 = new Book('The Silmarillion', 'J. R. R. Tolkien', '365', false);
 
-let toggleButton = new Button('toggle', 'Already read it?', 'toggle-button');
-let deleteButton = new Button('delete', 'Delete this book', 'delete-button');
-
 const tableBooks = document.getElementById('table-books');
 const addBookSection = document.getElementById('add-book');
 
@@ -52,7 +49,7 @@ function renderLibrary() {
   tableBooks.innerHTML = '';
 
   for (let i = 0; i < myLibrary.length; i++) {
-    let row = tableBooks.insertRow(0);
+    let row = tableBooks.insertRow(-1);
     row.setAttribute('data-book-index', i);
 
     fillRow(i, row);
@@ -60,17 +57,18 @@ function renderLibrary() {
 };
 
 /**
- * Function to create cells and fill with data
+ * Function to create cells and fill with data and icons
  */
 function fillRow(index, row) {
   let book = myLibrary[index];
   
   for (let property in book) {
-    if (book.hasOwnProperty(property) && typeof book[property] != 'function') {
+    if (book.hasOwnProperty(property) && property == 'read') {
+      insertIcon(index, row.insertCell(-1), book[property]);
+    } else if (book.hasOwnProperty(property) && typeof book[property] != 'function') {
       row.insertCell(-1).innerText = book[property];
     } else {
-      addButton(row.insertCell(-1), 'toggle');
-      addIcon(row.insertCell(-1), 'delete');
+      insertIcon(index, row.insertCell(-1), 'delete');
     }
   }
 };
@@ -79,45 +77,49 @@ function fillRow(index, row) {
  * Functions related to buttons
  */
 
-function addIcon(currentCell, icon) {
-  newIcon = document.createElement('a');
-  newIcon.setAttribute('class', 'material-icons');
-  newIcon.innerHTML = icon;
-
+function insertIcon(rowIndex, currentCell, type) {
+  let iconType = type.toString();
+  let newIcon = createIcon(rowIndex, iconType);
+  
   currentCell.appendChild(newIcon);
+};
 
-  newIcon.addEventListener('click', function() {
-    switch (icon) {
+function createIcon(index, type) {
+  let icon = document.createElement('a');
+  icon.setAttribute('data-book-index-delete', index);
+  icon.classList.add('material-icons');
+
+  switch (type) {
+    case 'delete':
+      icon.classList.add('delete');
+      icon.innerHTML = type;
+      break;
+    case 'true':
+      icon.classList.add('check');
+      icon.innerHTML = 'check';
+      break;
+    case 'false':
+      icon.classList.add('clear');
+      icon.innerHTML = 'clear';
+      break;
+  };
+
+  icon.addEventListener('click', function() {
+    switch (type) {
       case 'delete':
-        deleteBook(newIcon);
+        deleteBook(icon);
+        break;
+      case 'true':
+        toggleRead(icon);
+        break;
+      case 'false':
+        toggleRead(icon);
         break;
     }
   });
-};
 
-function addButton(currentCell, type) {
-  buttonTemplate = allButtons.find((button) => button.type === type );
-  newButton = buildButton(buttonTemplate);
-  currentCell.appendChild(newButton);
-};
-
-function buildButton(template) {
-  let button = document.createElement('button');
-  button.innerHTML = template.btnText;
-  button.classList.add(template.btnClass);
-
-  button.addEventListener('click', function() {
-    switch (template.type) {
-      case 'toggle':
-        toggleRead(button);
-        break;
-      case 'delete':
-        deleteBook(button);
-        break;
-    }
-  });
-  return button;
-};
+  return icon;
+}
 
 function toggleRead(button) {
   let targetRow = button.parentElement.parentElement;
@@ -132,6 +134,7 @@ function toggleRead(button) {
 
 function deleteBook(button) {
   let targetRow = button.parentElement.parentElement;
+
   let rowParent = targetRow.parentElement;
   if (confirm('Are you sure you want to delete this book?')) {
     rowParent.removeChild(targetRow);
